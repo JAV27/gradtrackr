a622831095ab5fcfd358d1c06649b9831e7b8b78
Josh Viktorov <joshviktorov27@gmail.com>
2020-05-11 12:40:45 -0400

    initial template

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const ejs = require('ejs')
 const PORT = 3000
 
-app.get('/', (req, res) => res.send('asdasd!'))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.render('index'))
 
 app.listen(PORT, function() {
     console.log("Server is running on port " + PORT);

7657c24c57e634758f3a8e973b315baca7591ae0
einarklarlund <einar.klarlund@gmail.com>
2020-05-27 17:57:10 -0400

    changed pdf files

--- scraping/scraping.js
+++ scraping/scraping.js
@@ -3,32 +3,32 @@ pdfjsLib.GlobalWorkerOptions.workerSrc =
 './pdf.worker.js';
 
 function gettext(pdfUrl){
-    var pdf = pdfjsLib.getDocument(pdfUrl);
-    return pdf.promise.then(function(pdf) { // get all pages text
-      var maxPages = pdf.numPages;
-      var countPromises = []; // collecting all page promises
-      for (var j = 1; j <= maxPages; j++) {
+var pdf = pdfjsLib.getDocument(pdfUrl);
+return pdf.promise.then(function(pdf) { // get all pages text
+        var maxPages = pdf.numPages;
+        var countPromises = []; // collecting all page promises
+        for (var j = 1; j <= maxPages; j++) {
         var page = pdf.getPage(j);
-  
+
         var txt = "";
         countPromises.push(page.then(function(page) { // add page promise
-          var textContent = page.getTextContent();
-          return textContent.then(function(text){ // return content promise
+            var textContent = page.getTextContent();
+            return textContent.then(function(text){ // return content promise
             return text.items.map(function (s) { return s.str; }).join(''); // value page text 
-          });
+            });
         }));
-      }
-      // Wait for all pages and join text
-      return Promise.all(countPromises).then(function (texts) {
+        }
+        // Wait for all pages and join text
+        return Promise.all(countPromises).then(function (texts) {
         return texts.join('');
-      });
+        });
     });
-  }
+}
   
-//   // waiting on gettext to finish completion, or error
+// waiting on gettext to finish completion, or error
 gettext("./PrereqInfo.pdf").then(function (text) {
     alert('parse ' + text);
-}, 
+},
     function (reason) {
     console.error(reason);
-})
+});
